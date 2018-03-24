4b38ebcb491bf264af55e7779b5f42895e4529b1
Anubhav <gupta.anubhav25@gmail.com>
2018-03-24 15:08:29 +0530

    fragments added

--- app/src/main/java/com/example/hp/lifeshare/BloodBankDetails/BloodBankActivity.java
+++ app/src/main/java/com/example/hp/lifeshare/BloodBankDetails/BloodBankActivity.java
@@ -1,8 +1,12 @@
 package com.example.hp.lifeshare.BloodBankDetails;
 
+import android.app.Fragment;
+import android.app.FragmentTransaction;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -13,6 +17,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.example.hp.lifeshare.R;
+
 public class BloodBankActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
@@ -22,16 +28,6 @@ public class BloodBankActivity extends AppCompatActivity
         setContentView(R.layout.activity_blood_bank);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -77,25 +73,55 @@ public class BloodBankActivity extends AppCompatActivity
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+       /* // Handle navigation view item clicks here.
         int id = item.getItemId();
+            if (id == R.id.currentDetails) {
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.updateDetails) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.issueBlood) {
 
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.history) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nearByUsers) {
 
-        } else if (id == R.id.nav_send) {
+        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);*/
+        return true;
+    }
+    private void displaySelectedScreen(int itemId) {
 
+        //creating fragment object
+        Fragment fragment = null;
+
+        //initializing the fragment object which is selected
+        switch (itemId) {
+            case R.id.currentDetails:
+                fragment = new currentDetails();
+                break;
+            case R.id.updateDetails:
+                fragment = new updateDetails();
+                break;
+            case R.id.issueBlood:
+                fragment = new issueBlood();
+                break;
+            case R.id.nearByUsers:
+                fragment = new nearByUsers();
+                break;
+            case R.id.history:
+                fragment = new history();
+                break;
+        }
+
+        //replacing the fragment
+        if (fragment != null) {
+            FragmentTransaction ft = getFragmentManager().beginTransaction();
+            ft.replace(R.id.content_frame, fragment);
+            ft.commit();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
---
+++ app/src/main/java/com/example/hp/lifeshare/BloodBankDetails/updateDetails.java
@@ -0,0 +1,30 @@
+package com.example.hp.lifeshare.BloodBankDetails;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.hp.lifeshare.R;
+
+/**
+ * Created by ANUBHAV on 24-Mar-18.
+ */
+
+public class updateDetails extends Fragment {    @Override
+public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    //returning our layout file
+    //change R.layout.yourlayoutfilename for each of your fragments
+    return inflater.inflate(R.layout.update_details_fragment, container, false);
+}
+
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Update Status");
+    }
+}
